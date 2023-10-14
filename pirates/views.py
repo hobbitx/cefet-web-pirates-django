e719c36f00aea9d1447db3a3531aacba5845ad87
Robert Cristiam <robert.cristian@live.com>
2023-10-14 10:26:51 -0300

    listar tesouros

--- pirates/views.py
+++ pirates/views.py
@@ -1,2 +1,18 @@
 from django.shortcuts import render
 from django.views import View
+from .models import Tesouro
+from django.db.models import F, ExpressionWrapper, DecimalField, Sum
+
+class ListaTesouros(View):
+     def get(self, request):
+
+        type_total_value = DecimalField(max_digits=10, decimal_places=2)
+        total_value = ExpressionWrapper(F('preco') * F('quantidade'), output_field=type_total_value)
+
+        tesouros = Tesouro.objects.annotate(valor_total=total_value)
+
+        
+        context = {'lista_tesouros': tesouros}
+        context.update(tesouros.aggregate(total_geral=Sum('valor_total', output_field=type_total_value)))
+
+        return render(request, 'lista_tesouros.html', context)
